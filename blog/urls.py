8d7ab05ffffe1812241db1c7e7a3be813366c3ee
D0683497 <ff129043923@gmail.com>
2019-03-10 15:33:39 +0800

    新增註解

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('create', views.post_create, name='post_create'),
     path('additional', views.course_additional, name='course_additional')
+    # 前面是網址部分，最後是模板要用的url
 ]
